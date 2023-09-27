991167ce621b38e2583e2bf05f5c7cc7a06261e8
Chris022 <christoph02.cw@gmail.com>
2023-09-27 19:14:48 +0200

    implement algorithms for typeseting paragraphs

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from lib import *
+from fpdf import FPDF
+
+new_document((400,50))
+new_page()
+
+set_font("Arial","B",10)
+set_cursor(0,8)
+textblock_center_aligned(100,10,"hallo ich bin der beste mensch der welt nicht. Da muss mann auch mal ehrlich sein!")
+
+export()
